55e12df96556a197d46c4e1fc3b58d53baea4bd8
PeresvetKK <koltyrinpk@gmail.com>
2023-06-05 15:17:41 +0400

    страница дневника

--- src/pages/Journal/Journal.jsx
+++ src/pages/Journal/Journal.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from 'react';
+import Table from '../../components/Table/Table';
+import s from './Journal.module.scss';
+import { LeftArrowSVG, RightArrowSVG } from '../../resources/svg';
 
 const Journal = () => {
-  return (
-    <div>Journal</div>
-  )
+    return (
+        <section className="section">
+            <div className={s["journal-header"]}>
+                <h1>Дневник 22.05.2023 - 29.05.2023</h1>
+                <div className='arrows-box'>
+                    <LeftArrowSVG/>
+                    <RightArrowSVG/>
+                </div>
+            </div>
+            <div className={`${s['table-section']}`}>
+                <Table />
+                <Table />
+            </div>
+        </section>
+    )
 }
 
 export default Journal
